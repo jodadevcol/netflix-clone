4e20c5c141eb142123a6782757662e81bf951da4
Daniel García <dagardev.col@gmail.com>
2022-02-15 21:13:50 -0500

    [ADD] Components Icons of app

---
+++ src/components/Icons/ArrowRight.jsx
@@ -0,0 +1,16 @@
+export const ArrowRight = ({ classname, fill = 'currentColor' }) => (
+  <svg
+    className={classname}
+    viewBox="0 0 6 12"
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+  >
+    <path
+      d="m.61 1.312.78-.624L5.64 6l-4.25 5.312-.78-.624L4.36 6z"
+      fill={fill}
+    />
+  </svg>
+)
+
+
