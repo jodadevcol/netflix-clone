0b95938b9c833a8a3186ea93b43478780b0a0bd6
Daniel García <dagardev.col@gmail.com>
2022-02-16 18:43:12 -0500

    [ADD] Component Icons is Closed

---
+++ src/components/Icons/Closed.jsx
@@ -0,0 +1,11 @@
+export const Closed = ({ className, fill = "currentColor" }) => (
+  <svg
+    className={className}
+    viewBox="0 0 26 26"
+    width="1em"
+    height="1em"
+    fill={fill}
+  >
+    <path d="M10.5 9.3 1.8.5.5 1.8l8.8 8.7-8.8 8.8 1.3 1.2 8.7-8.7 8.8 8.7 1.2-1.2-8.7-8.8 8.7-8.7L19.3.5l-8.8 8.8Z" />
+  </svg>
+)
